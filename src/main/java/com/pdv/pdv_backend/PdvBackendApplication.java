eece74c6d3d6d2065aa5278c43e2857d1feec4f5
angelherreraP <a.manuel.herrera.p@gmail.com>
2026-09-19 17:48:13 -0600

    Setup inicial Spring Boot

---
+++ src/main/java/com/pdv/pdv_backend/PdvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pdv.pdv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvBackendApplication.class, args);
+	}
+
+}
